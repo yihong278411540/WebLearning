70856da855dccc39b0a3d73589deb273d0e53e81
yh <278411540@qq.com>
2023-05-06 13:51:17 +0800

    0506-learn wxprogrammini基础-05

---
+++ wxProjectsMini/mp_05/store/store.js
@@ -0,0 +1,18 @@
+//在这个js文件里，专门来创建store实例对象
+import { observable, action} from 'mobx-miniprogram'
+
+export const store = observable({
+	numA: 1,
+	numB:2,
+	//计算属性
+	get sum() {
+		return this.numA + this.numB
+	},
+	//actions方法，用来修改store中的数据
+	updateNum1: action(function(step) {
+		this.numA += step
+	}),
+	updateNum2: action(function(step) {
+		this.numB += step
+	}),
+})
