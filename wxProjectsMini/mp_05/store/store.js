6697290147f743a642e997b793d51a279ef6ba8d
yh <278411540@qq.com>
2023-05-06 17:01:18 +0800

    0506-learn wxprogrammini基础-05自定义Tabbar

--- wxProjectsMini/mp_05/store/store.js
+++ wxProjectsMini/mp_05/store/store.js
@@ -4,6 +4,7 @@ import { observable, action} from 'mobx-miniprogram'
 export const store = observable({
 	numA: 1,
 	numB:2,
+	activeTabBarIndex: 0,
 	//计算属性
 	get sum() {
 		return this.numA + this.numB
@@ -15,4 +16,7 @@ export const store = observable({
 	updateNum2: action(function(step) {
 		this.numB += step
 	}),
+	updateActiveTabBarIndex: action(function(index) {
+		this.activeTabBarIndex = index
+	})
 })
